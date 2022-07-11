a0bb9ccece31222f4b3ede7e149e71930f4bfca4
Abhishek <jangal&mangal.gov>
2022-07-11 19:03:16 +0530

    scroll added

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ const mobileNav =document.querySelector('.mobile-nav')
 const nav_content =document.querySelectorAll('.content-container-nav')
 
 
-
-
 function hamburger_toggle(){
   hamburger.classList.toggle('active');
   mobileNav.classList.toggle('active');
 }
 
 hamburger.addEventListener('click',hamburger_toggle)
-
-
 
 for (let index = 0; index < nav_content.length; index++) {
     nav_content[index].addEventListener('click',function(){
